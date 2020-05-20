d4327bc9b5761d38f3a1daee6b29cff99b6e6bca
Irina Nicula <inicula@mobiquityinc.com>
2020-05-20 16:41:12 +0200

    Added styling to main page, displayed all necessary data and added a title separate component

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import './MainPage.scss';
+import logo from '../../assets/logo.png';
+import PageTitle from "../PageTitle/PageTitle";
 
 const MainPage = () => {
   let [data, setData] = useState([]);
@@ -11,27 +14,42 @@ const MainPage = () => {
       //returning all the data, including year and champion information
       .then((jsonResponse) => {
         //figure out why this works better
-        return setData(jsonResponse.MRData.StandingsTable.StandingsLists.filter(champion => champion.season >= 2005));   
-      })
-  }
+        return setData(
+          jsonResponse.MRData.StandingsTable.StandingsLists.filter(
+            (champion) => champion.season >= 2005
+          )
+        );
+    });
+  };
 
-    //research useEffect
-    useEffect(() => {
-        getChampionsData();
-    }, []);
+  //research useEffect
+  useEffect(() => {
+    getChampionsData();
+  }, []);
 
   return (
     <div>
+        <PageTitle 
+            title="champions from 2005 to present" 
+            logo={logo}
+        />
         <table>
-            <tbody>
-                {console.log(data.map(datum => datum.DriverStandings[0].Driver))}
-                {
-                    data.map(datum => {
-                        return <tr key={datum.season}>
-                                    <td>{datum.season}</td>
-                                    <td>{datum.DriverStandings[0].Driver.givenName} {datum.DriverStandings[0].Driver.familyName}</td>
-                                </tr>
-                })}
+            <tbody className="champions-table">
+            <tr className="champions-table__header">
+                <th>Year</th>
+                <th>Champion</th>
+            </tr>
+            {data.map((datum) => {
+                return (
+                <tr className = "champions-table__row" key={datum.season}>
+                    <td>{datum.season}</td>
+                    <td>
+                    {datum.DriverStandings[0].Driver.givenName}{" "}
+                    {datum.DriverStandings[0].Driver.familyName}
+                    </td>
+                </tr>
+                );
+            })}
             </tbody>
         </table>
     </div>
