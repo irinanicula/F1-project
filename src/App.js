fb1eedf9809c66846f4ef13ea11ef11624f161ec
Irina Nicula <inicula@mobiquityinc.com>
2020-06-19 11:08:45 +0200

    Added PageLayout component to remove responsibility from App

--- src/App.js
+++ src/App.js
@@ -1,51 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './stylesheets/main.scss';
-import MainPage from './pages/MainPage/MainPage';
-import SeasonWinners from './pages/SeasonWinnersPage/SeasonWinners';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { getChampionsData, getWinnersData } from './services/getDrivers';
+import PageLayout from './layouts/PageLayout/PageLayout';
 
 const App = () => {
-   let [championsData, setChampionsData] = useState([]);
-   let [winnersData, setWinnersData] = useState([]);
-   let [error, setError] = useState('');
-
-   const handleClick = (season) => {
-      getWinnersData(season, setWinnersData, setError);
-   };
-
-   //provide an empty array as second argument to the effect hook to avoid activating it on component updates but only for the mounting of the component
-   useEffect(() => {
-      getChampionsData(setChampionsData, setError);
-   }, []);
-
-   return (
-      <div className="app" data-testid="app">
-         {/* uses the HTML5 History API */}
-         <BrowserRouter>
-            <Switch>
-               <Route exact path="/">
-                  {error ? (
-                     <h1>
-                        {error.name} : {error.message}
-                     </h1>
-                  ) : (
-                     <MainPage champions={championsData} handleClick={handleClick} />
-                  )}
-               </Route>
-               <Route path="/season/winners">
-                  {error ? (
-                     <h1>
-                        {error.name} : {error.message}
-                     </h1>
-                  ) : (
-                     <SeasonWinners winners={winnersData} />
-                  )}
-               </Route>
-            </Switch>
-         </BrowserRouter>
-      </div>
-   );
+   return <PageLayout />;
 };
 
 export default App;
---
+++ src/layouts/PageLayout/PageLayout.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import '../../stylesheets/main.scss';
+import MainPage from '../../pages/MainPage/MainPage';
+import SeasonWinners from '../../pages/SeasonWinnersPage/SeasonWinners';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { getChampionsData, getWinnersData } from '../../services/getDrivers';
+
+const PageLayout = () => {
+   let [championsData, setChampionsData] = useState([]);
+   let [winnersData, setWinnersData] = useState([]);
+   let [error, setError] = useState('');
+
+   const handleClick = (season) => {
+      getWinnersData(season, setWinnersData, setError);
+   };
+
+   //provide an empty array as second argument to the effect hook to avoid activating it on component updates but only for the mounting of the component
+   useEffect(() => {
+      getChampionsData(setChampionsData, setError);
+   }, []);
+
+   return (
+      <div className="page-layout" data-testid="page-layout">
+         {/* uses the HTML5 History API */}
+         <BrowserRouter>
+            <Switch>
+               <Route exact path="/">
+                  {error ? (
+                     <h1>
+                        {error.name} : {error.message}
+                     </h1>
+                  ) : (
+                     <MainPage champions={championsData} handleClick={handleClick} />
+                  )}
+               </Route>
+               <Route path="/season/winners">
+                  {error ? (
+                     <h1>
+                        {error.name} : {error.message}
+                     </h1>
+                  ) : (
+                     <SeasonWinners winners={winnersData} />
+                  )}
+               </Route>
+            </Switch>
+         </BrowserRouter>
+      </div>
+   );
+};
+
+export default PageLayout;
